257a3640d0837f8f09b4a8cb3b9239c2b0841d4b
Denis442 <denisdaydeneg@mail.ru>
2022-10-30 20:04:52 +0300

    add seeders

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,6 @@
 <script>
-
 import Card from "../components/UI/Card.vue"
-
-import Wizard from "../assets/img/wizard.png"
-import Archer from "../assets/img/archer.png"
-import Goblin from "../assets/img/goblin.png"
+import items from '../seeders/items.js'
 
 export default {
 components:{
@@ -13,35 +9,12 @@ components:{
 
 data(){
     return {
-        items: [
-            {
-                id: 1,
-                title: 'Archer',
-                descr: 'archer archer archer archer',
-                img: Archer,
-                lvl: 4
-            },
-            {
-                id: 2,
-                title: 'Wizard',
-                descr: 'Wizard Wizard Wizard Wizard',
-                img: Wizard,
-                lvl: 5
-            },
-            {
-                id: 3,
-                title: 'Goblin',
-                descr: 'archer archer archer archer',
-                img: Goblin,
-                lvl: 4
-            }
-        ],
-        image: Wizard
+        items: items
     }
-}
-    
+}  
 }
 </script>
+
 <template>
 
     <div class="card__wrapper" v-for="item in items" :key="item.id">
---
+++ src/seeders/items.js
@@ -0,0 +1,27 @@
+import Wizard from "../assets/img/wizard.png"
+import Archer from "../assets/img/archer.png"
+import Goblin from "../assets/img/goblin.png"
+
+export default [
+    {
+        id: 1,
+        title: 'Archer',
+        descr: 'archer archer archer archer',
+        img: Archer,
+        lvl: 4
+    },
+    {
+        id: 2,
+        title: 'Wizard',
+        descr: 'Wizard Wizard Wizard Wizard',
+        img: Wizard,
+        lvl: 5
+    },
+    {
+        id: 3,
+        title: 'Goblin',
+        descr: 'archer archer archer archer',
+        img: Goblin,
+        lvl: 4
+    }
+]
